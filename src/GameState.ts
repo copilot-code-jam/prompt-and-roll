1bfd306658868a12574cbf9c5fbaf3cd0d59aed2
Joseph Rozencwajg <joseph.rozencwajg@doctolib.com>
2025-03-12 10:32:35 +0100

    game state management

---
+++ src/GameState.ts
@@ -0,0 +1,71 @@
+type GameStatus = 'PLAY' | 'GAMEOVER' | 'WON';
+
+class GameState {
+    private totalCoins: number;
+    private currentLevel: number;
+    private status: GameStatus;
+    private earnStreak: number;
+    private loseStreak: number;
+
+    private static minCoinsPerLevel = new Map<number, number>([
+        [1, 10],
+        [2, 20],
+        [3, 30]
+    ]);
+
+    private static rocksPerLevel = new Map<number, number>([
+        [1, 5],
+        [2, 10],
+        [3, 15]
+    ]);
+
+    constructor() {
+        this.totalCoins = 0;
+        this.currentLevel = 1;
+        this.status = 'PLAY';
+        this.earnStreak = 0;
+        this.loseStreak = 0;
+    }
+
+    public setTotalCoins(coins: number): void {
+        this.totalCoins = coins;
+    }
+
+    public getTotalCoins(): number {
+        return this.totalCoins;
+    }
+
+    public setCurrentLevel(level: number): void {
+        this.currentLevel = level;
+    }
+
+    public getCurrentLevel(): number {
+        return this.currentLevel;
+    }
+
+    public setStatus(status: GameStatus): void {
+        this.status = status;
+    }
+
+    public getStatus(): GameStatus {
+        return this.status;
+    }
+
+    public resetEarnStreak(): void {
+        this.earnStreak = 0;
+    }
+
+    public resetLoseStreak(): void {
+        this.loseStreak = 0;
+    }
+
+    public static getMinCoinsForLevel(level: number): number {
+        return this.minCoinsPerLevel.get(level) || 0;
+    }
+
+    public static getRocksForLevel(level: number): number {
+        return this.rocksPerLevel.get(level) || 0;
+    }
+}
+
+export default GameState;
